d940a11741e4d5106067df96f3f492bc1f44479f
Ermondel <ermondel@gmail.com>
2019-11-21 16:54:09 +0200

    feat: update structure to new version

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
-![Node.js ](https://github.com/ermondel/tsttmp/blob/master/files/icons48b/Nodejs48v2.png)
-![Express.js](https://github.com/ermondel/tsttmp/blob/master/files/WebFramework48b.png)
+![Node.js ](https://github.com/ermondel/tsttmp/blob/master/files/icons48b/Nodejs48v2.png) ![Express.js](https://github.com/ermondel/tsttmp/blob/master/files/WebFramework48b.png)
 
-## Node web server
+# Node weather server
 
 _Project for Udemy NodeJS course._
 
 ### Technologies
 
-| name       | description                                    |
-| ---------- | ---------------------------------------------- |
-| Node.js    | cross-platform JavaScript run-time environment |
-| Express.js | web application framework                      |
+- Node
+- Express
--- src/app.js
+++ src/app.js
@@ -1,21 +1,55 @@
 const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
 
 const app = express();
 
-app.get('', (req, res) => {
-  res.send('Index');
-});
+const pathViews = '../templates/views';
+const pathPublic = '../public';
+const pathPartials = '../templates/partials';
 
-app.get('/help', (req, res) => {
-  res.send('Help page');
+app.set('views', path.join(__dirname, pathViews));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, pathPublic)));
+hbs.registerPartials(path.join(__dirname, pathPartials));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about', {
+    title: 'About',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+  });
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page');
+  res.send({
+    forecast: 'It is snowing.',
+    location: 'Antarctica',
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: 'Help',
+    message: 'Help article not found',
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    message: 'Page not found',
+  });
 });
 
 app.listen(3000, () => {
